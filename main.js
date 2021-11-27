caf6fa76a3a9766cfe726bf971e160b39f10609f
Dr127 <79019262+Dr127@users.noreply.github.com>
2021-11-26 19:11:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 ="";
 song2 = "";
+leftWristX =0;
+leftWristY = 0;
+rightWristX =0;
+rightWristY = 0;
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -10,7 +14,27 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,650,500)
+    circle(leftWristX,leftWristY,20,20)
+    circle(rightWristX,rightWristY,20);
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX+"leftWristY =" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX+"rightWristY =" + rightWristY);
+    }
 }
