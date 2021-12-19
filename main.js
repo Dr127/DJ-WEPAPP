790ac218f04c4d312cf09b8885456037e6d17acf
Dr127 <79019262+Dr127@users.noreply.github.com>
2021-12-19 17:21:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ song2 = "";
 leftWristX =0;
 leftWristY = 0;
 leftWristScore = 0;
-
+rightWristScore = 0;
 rightWristX =0;
 rightWristY = 0;
+
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -27,15 +28,24 @@ function draw(){
     fill("FF00000");
 
     status1=song1.isPlaying();
-
+    status2=song2.isPlaying();
+    if(rightWristScore > 0.2){
+        circle(rightWristX,rightWristY,20,20) 
+        song1.stop();
+        if(status2 == false){
+            song2.play();
+            document.getElementById("name").innerHTML = "Song Playing is Song 2";
+        }
+    }
     if(leftWristScore>0.2){
         circle(leftWristX,leftWristY,20,20)
         song2.stop();
+        if(status1 == false){
+            song1.play();
+            document.getElementById("name").innerHTML = "Song Playing is Song 1";
+        }
     } 
-    if(status1 == false){
-        song1.play();
-        document.getElementById("name").innerHTML = "Song Playing is Song 1";
-    }
+    
 }
 function modelLoaded(){
     console.log('PoseNet is Initialized');
@@ -45,6 +55,7 @@ function gotPoses(results){
     
         console.log(results);
         leftWristScore = results[0].pose.keypoints[9].score;
+        rightWristScore = results[0].pose.keypoints[10].score;
         console.log("scoreLeftWrist = " + leftWristScore);
 
         leftWristX = results[0].pose.leftWrist.x;
